f5fadd942cd159b8faa6dbda2b27e8623ea7cd53
shashimal <shasimald@gmail.com>
2018-12-01 21:03:14 +0800

    Enabled discovery client and service

--- web-app/src/main/java/com/duleendra/webapp/WebAppApplication.java
+++ web-app/src/main/java/com/duleendra/webapp/WebAppApplication.java
@@ -1,16 +1,14 @@
-package com.duleendra.configserver;
+package com.duleendra.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigServerApplication {
+public class WebAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(WebAppApplication.class, args);
     }
 }
